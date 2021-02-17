8c8098348968c62d77234ab06fe4c6e1af6391a1
Bram Renting <bramrenting@gmail.com>
2021-02-17 14:42:41 +0100

    moved auxiliary files to scripts folder

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 import tensorflow_probability as tfp
 from tqdm.notebook import tqdm
@@ -8,11 +7,13 @@ from mixmatch import (ema, interleave, linear_rampup, mixmatch, semi_loss,
 
 
 def train(datasetX, datasetU, model, ema_model, optimizer, epoch, args):
+    # performance measures
     xe_loss_avg = tf.keras.metrics.Mean()
     l2u_loss_avg = tf.keras.metrics.Mean()
     total_loss_avg = tf.keras.metrics.Mean()
     accuracy = tf.keras.metrics.CategoricalAccuracy()
 
+    # batch function
     shuffle_and_batch = lambda dataset: dataset.shuffle(buffer_size=int(1e6)).batch(batch_size=args['batch_size'], drop_remainder=True)
 
     iteratorX = iter(shuffle_and_batch(datasetX))
@@ -69,20 +70,23 @@ def train(datasetX, datasetU, model, ema_model, optimizer, epoch, args):
             'Accuracy': f'{accuracy.result():.3%}'
         })
 
-    return xe_loss_avg, l2u_loss_avg, total_loss_avg, accuracy
-
 
 def validate(dataset, model, epoch, args, split):
+    # performance metrics
     accuracy = tf.keras.metrics.CategoricalAccuracy()
     xe_avg = tf.keras.metrics.Mean()
 
-    dataset = dataset.batch(args['batch_size'])
-    for batch in dataset:
+
+    for batch in dataset.batch(args['batch_size']):
+        # predict
         logits = model(tf.cast(batch['image'], dtype=tf.float32), training=False)
-        xe_loss = tf.nn.softmax_cross_entropy_with_logits(labels=tf.one_hot(batch['label'], 10), logits=logits)
-        xe_loss = tf.reduce_mean(xe_loss)
+
+        # calculate loss
+        xe_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf.one_hot(batch['label'], 10), logits=logits))
+
+        # update performance measures
         xe_avg.update_state(xe_loss)
         accuracy.update_state(tf.one_hot(batch['label'], 10), logits)
-    print(f'Epoch {epoch:04d}: {split} XE Loss: {xe_avg.result():.4f}, {split} Accuracy: {accuracy.result():.3%}')
 
-    return xe_avg, accuracy
+    # print stats
+    print(f'Epoch {epoch:04d}: {split} XE Loss: {xe_avg.result():.4f}, {split} Accuracy: {accuracy.result():.3%}')
